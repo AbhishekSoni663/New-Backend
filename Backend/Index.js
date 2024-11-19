1e4c78a07509117f201e682f9e66a4f032ed64c5
AbhishekSoni663 <Abhisheksoni663@gmail.com>
2024-11-20 00:06:16 +0530

    fifth commit

--- Backend/Index.js
+++ Backend/Index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 require('dotenv').config()
 
+const app = express()
 const PORT = process.env.PORT || 3000
 
-const app = express()
+app.use(express.json())
+app.use('/', require('./TodoRoutes/TodoRoutes'))
+
 
 app.listen(PORT, ()=>{
     console.log(`Server is running at Port : ${PORT}`)
 })
 
-app.use('/', require('./TodoRoutes/TodoRoutes'))
-app.use('/todo', require("./TodoRoutes/TodoRoutes"));
