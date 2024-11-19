182bdb2d1b7a35a8ad9f6daf83c27545cc7c7513
AbhishekSoni663 <Abhisheksoni663@gmail.com>
2024-11-20 00:49:32 +0530

    seventh commit

--- Backend/Index.js
+++ Backend/Index.js
@@ -7,6 +7,9 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use('/', require('./TodoRoutes/TodoRoutes'))
 
+app.get('/', (req, res) => {
+    res.send('Hello from the backend!');
+});
 
 app.listen(port, ()=>{
     console.log(`Server is running at Port : ${port}`)
